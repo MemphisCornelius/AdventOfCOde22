12ec8f6580f58d7ce6a84990c67da38f273ab3c1
MemphisCornelius <m.krich@online.de>
2022-12-03 13:23:50 +0100

    Added solution for day01
    Signed-off-by: MemphisCornelius <m.krich@online.de>

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,41 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{AddCallories(_input).Max()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"{GetTop3()}2");
+
+    private int GetTop3()
+    {
+        List<int> elves = AddCallories(_input);
+        elves.Sort();
+        return (elves[^1] + elves[^2] + elves[^3]);
+    }
+
+    private List<int> AddCallories(string[] lines)
+    {
+        List<int> elves = new();
+        int calories = 0;
+
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                elves.Add(calories);
+                calories = 0;
+            } else
+            {
+                calories += Convert.ToInt32(line);
+            }
+        }
+
+        return elves;
+    }
 }
