18ae7d5f6031ee82d535149ebd98c88ef92519e4
MemphisCornelius <m.krich@online.de>
2022-12-04 13:55:48 +0100

    Added solution for day04 part2

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -27,11 +27,27 @@ public class Day04 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        int sum = 0;
+
+        foreach (string s in _input)
+        {
+            string[] ranges = s.Split(",");
+            string[] elve1 = ranges[0].Split("-");
+            string[] elve2 = ranges[1].Split("-");
+
+            if (DoOverlap(int.Parse(elve1[0]), int.Parse(elve1[1]), int.Parse(elve2[0]), int.Parse(elve2[1])))
+                sum++;
+        }
+        return new($"{sum}");
     }
-    
+
     private bool ContainsRange(int lower1, int upper1, int lower2, int upper2)
     {
         return ((lower1 <= lower2 && upper1 >= upper2) || (lower2 <= lower1 && upper2 >= upper1));
+    }
+    
+    private bool DoOverlap(int lower1, int upper1, int lower2, int upper2)
+    {
+        return ((upper1 >= lower2 && upper1 <= upper2) || upper2 >= lower1 && upper2 <= upper1);
     }
 }
